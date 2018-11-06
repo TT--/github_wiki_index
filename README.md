7bd3116eeeee0c5f736e1a46b8fe2bd3d7665d56
TT-- <TT--@users.noreply.github.com>
2018-11-05 23:30:48 -0500

    Link files in the current directory as well, but don't link to "_Sidebar" or "Home", add bold headings

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
-Creates an index with links from a github wiki which is structured in folders. An example could be found in the wiki of this page where the index will be created in the _Sidebar.md.
 
-![index example](https://raw.githubusercontent.com/bitmovin/github_wiki_index/master/index.png "index example")
 
-## How does it work?
+forked from [bitmovin/github_wiki_index](https://github.com/bitmovin/github_wiki_index)
 
-- Clone your wiki git repo (e.g., git clone https://github.com/bitmovin/github_wiki_index.wiki.git)
-- Move mk_index.py into the folder of the wiki
-- Execute mk_index.py with _python mk_index.py_
-- Now you should see a _Sidebar.md with the index generated from your folder and file structure
+This modification also links files in the current directory (.) in the created `_Sidebar.md`
+
+
--- mk_index.py
+++ mk_index.py
@@ -1,18 +1,53 @@
 import os
 
-def createIndexFile(startpath, indexFile):
-    for root, dirs, files in os.walk(startpath):
-        files = [f for f in files if not f[0] == '.']
-        dirs[:] = [d for d in dirs if not d[0] == '.']
-        level = root.replace(startpath, '').count(os.sep) - 1
-        indent = ' ' * 2 * (level)
-        directory = os.path.basename(root)
-        if directory != '.':
-            indexFile.write('{}- {}\n'.format(indent, os.path.basename(root)))
-            subindent = ' ' * 2 * (level + 1)
-            for f in files:
-                indexFile.write('{}- [{}]({})\n'.format(subindent, f[:-3], f[:-3]))
 
-indexFile = open('_Sidebar.md', 'w')
-createIndexFile(".", indexFile)
-indexFile.close();
+def create_index_file(startpath, index_file):
+    # os.walk
+    # Generate a 3-tuple (dirpath, dirnames, filenames) by walking the tree either top-down or bottom-up.
+    # - dirpath is a string, the path to the directory.
+    # - dirnames is a list of the names of the subdirectories in dirpath (excluding '.' and '..').
+    # - filenames is a list of the names of the non-directory files in dirpath.
+    #
+    # Note that the names in the lists contain no path components.
+    # To get a full path (which begins with top) to a file or directory in dirpath, do:
+    #   os.path.join(dirpath, name).
+
+    for root, dirs, files in os.walk(startpath):
+
+        # ignore files starting with '.'
+        # files = [f for f in files if not f[0] == '.']
+
+        # ignore directories starting with '.'
+        dirs[:] = [d for d in dirs if not d[0] == '.']
+
+        level = root.replace(startpath, '').count(os.sep) - 1
+        indent = ' ' * 2 * level
+
+        dir_path = os.path.basename(root)
+        # if NOT ROOT directories:
+        if dir_path != '.':
+            # WRITE the Directory name
+            #  bold     index_file.write('\n{}**{}**\n'.format(indent, dir_path))
+            #  as a heading
+            index_file.write('\n{}### {}\n'.format(indent, dir_path))
+            # set the indent amount
+            subindent = ' ' * 2 * (level + 1)
+            # WRITE the filename
+            for f in files:
+                index_file.write('{}- [{}]({})\n'.format(subindent, f[:-3].replace('-', ' '), f[:-3]))
+        # ROOT directory '.'
+        else:
+            for f in files:
+                # don't link to '_Sidebar.md' or 'Home.md' in the sidebar
+                if f == '_Sidebar.md':
+                    continue
+                if f == 'Home.md':
+                    continue
+                # WRITE the filename
+                # bold
+                index_file.write('{}- **[{}]({})**\n'.format('', f[:-3].replace('-', ' '), f[:-3]))
+
+
+outputFile = open('_Sidebar.md', 'w')
+create_index_file(".", outputFile)
+outputFile.close()
